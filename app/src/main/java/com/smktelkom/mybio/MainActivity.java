6c2d32b8ddcb78ef073e26c3ba30afcdd386b085
fhrzdty31 <rezacts01@gmail.com>
2023-05-26 09:54:58 +0700

    Update

--- app/src/main/java/com/smktelkom/mybio/MainActivity.java
+++ app/src/main/java/com/smktelkom/mybio/MainActivity.java
@@ -1,15 +1,15 @@
 package com.smktelkom.mybio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     DatePickerDialog datePickerDialog;
     SimpleDateFormat dateFormatter;
 
+    @SuppressLint("SimpleDateFormat")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setTanggalLahir = (EditText) findViewById(R.id.setTanggalLahir);
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
-        setTanggalLahir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDateDialog();
-            }
-        });
+        setTanggalLahir.setOnClickListener(v -> showDateDialog());
 
         setNama = findViewById(R.id.setNama);
         setKelas = findViewById(R.id.setKelas);
@@ -47,28 +43,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnTampil.setOnClickListener(this);
 
         Button btnHapus = (Button) findViewById(R.id.button2);
-        btnHapus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setNama.setText("");
-                setKelas.setText("");
-                setTempatLahir.setText("");
-                setTanggalLahir.setText("");
-                setAlamat.setText("");
-                setHobi.setText("");
-            }
+        btnHapus.setOnClickListener(view -> {
+            setNama.setText("");
+            setKelas.setText("");
+            setTempatLahir.setText("");
+            setTanggalLahir.setText("");
+            setAlamat.setText("");
+            setHobi.setText("");
         });
     }
 
     private void showDateDialog() {
         Calendar calendar = Calendar.getInstance();
-        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-                Calendar newDate = Calendar.getInstance();
-                newDate.set(year, month, dayOfMonth);
-                setTanggalLahir.setText(dateFormatter.format(newDate.getTime()));
-            }
+        datePickerDialog = new DatePickerDialog(this, (datePicker, year, month, dayOfMonth) -> {
+            Calendar newDate = Calendar.getInstance();
+            newDate.set(year, month, dayOfMonth);
+            setTanggalLahir.setText(dateFormatter.format(newDate.getTime()));
         }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
         datePickerDialog.show();
     }
